756dafb048d47de33bdc8a697bb1bb7a4762688c
this-sunil <swarajya888@gmail.com>
2025-11-24 15:05:07 +0530

    save changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,26 +1,13 @@
-import UIKit
 import Flutter
+import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-
-    private var channel: FlutterMethodChannel?
-
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-
-        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-
-        // Initialize the channel
-        channel = FlutterMethodChannel(
-            name: "CheckoutProChannel",
-            binaryMessenger: controller.binaryMessenger
-        )
-
-        GeneratedPluginRegistrant.register(with: self)
-
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
